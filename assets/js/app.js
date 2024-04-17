6039b58be029490bab6367bf5fa666062f49d6e5
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-04-17 15:59:22 +0200

    add a test to check the api call

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,19 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            $testJS: [],
+            api_url: 'script.php'
         }
+    },
+    mounted() {
+        axios
+            .get(this.api_url)
+            .then(resp => {
+                console.log(resp);
+                this.$testJS = resp.data;
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
     }
 }).mount('#app')
